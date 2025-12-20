aa8f90a3936f9622cfd3ed0ba7452a6b30a5689c
anhgalxag2004 <anhgalxag2004@gmail.com>
2025-12-20 12:00:00 +0700

    fixx bug

--- frontend/components/layout/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Component Exports
- * Central export file for all layout components
- */
-
-export { default as AppShell } from './AppShell';
-export { default as Card } from './Card';
-export { default as Button } from './Button';
-export { default as FormField, TextAreaField } from './FormField';
